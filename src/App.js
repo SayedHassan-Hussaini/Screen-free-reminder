269518bafd4a2dc15e4d2cccac80ecfd06ce63d5
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-02-15 12:14:55 +0430

    setup project

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button>test</Button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
